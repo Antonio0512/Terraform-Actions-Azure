05844935941c8b807ebee1a328f5f34882754994
Antonio <antonioboyanov9@gmail.com>
2023-06-14 20:47:04 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
   
   backend "azurerm" {
     resource_group_name  = "StorageRG"
-    storage_account_name = "taskboardstoragemarto"
+    storage_account_name = "taskboardstorage"
     container_name       = "taskboardcontainer"
     key                  = "terraform.tfstate"
   }
